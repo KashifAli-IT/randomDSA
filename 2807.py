36f7fa2c5baf448734b8eca9e0d3565a6d1f44ce
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-09 22:40:21 +0500

    Add GCD method and insert function for linked list
    Implement GCD calculation and insertion in linked list.

---
+++ 2807.py
@@ -0,0 +1,23 @@
+class Solution:
+    def gcd(num1,num2):
+        div=min(num1,num2)
+        while num1%div!=0 or num2%div!=0:
+            div-=1
+        return div
+        
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next==None:
+            return head
+        
+        prev=head
+        temp=head.next
+        div=0
+
+        while temp!=None:
+            div=gcd(prev.val,temp.val)
+            node=ListNode(div,prev.next)
+            prev.next=node
+            prev=temp
+            temp=temp.next
+
+        return head
