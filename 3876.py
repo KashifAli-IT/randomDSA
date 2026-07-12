8a37924b02decd8bd8c0a548677a75b21e053f76
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-12 21:34:59 +0500

    Add uniformArray method to Solution class

---
+++ 3876.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        return min(nums1)%2 == 1 or all(x%2 == 0 for x in nums1)
