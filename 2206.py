89a5ae18a64e9dd7203553be826df2446c0dc241
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-16 22:18:54 +0500

    Implement divideArray method to check even frequency

---
+++ 2206.py
@@ -0,0 +1,12 @@
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return all(cnt%2 == 0 for cnt in collections.Counter(nums).itervalues())
