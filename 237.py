4d313bdafddbcb51bbe21f5c579d7f4a5a6a2d3e
Kashif Ali <146372614+KashifAli-IT@users.noreply.github.com>
2026-07-23 20:43:31 +0500

    Implement deleteNode method in Solution class

---
+++ 237.py
@@ -0,0 +1,10 @@
+
+class Solution(object):
+    # @param {ListNode} node
+    # @return {void} Do not return anything, modify node in-place instead.
+    def deleteNode(self, node):
+        if node and node.next:
+            node_to_delete = node.next
+            node.val = node_to_delete.val
+            node.next = node_to_delete.next
+            del node_to_delete
